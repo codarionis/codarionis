d7f6abf867dd5fd21eea66dc6e1df06dd3aaf0e8
blazexnz <lost_homie@hotmail.com>
2025-08-26 20:36:58 +1200

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ but deliver us from evil. Amen.`,
   hailMary: `<strong>V.</strong> Hail Mary, full of grace,  
 the Lord is with thee;  
 blessed art thou among women,  
-and blessed is the fruit of thy womb, Jesus. <br>
+and blessed is the fruit of thy womb, Jesus.<br>
 <strong>R.</strong> Holy Mary, Mother of God,  
 pray for us sinners,  
 now and at the hour of our death. Amen.`,
@@ -1372,3 +1372,4 @@ toggleBeadsBtn.addEventListener("click", () => {
   const beadsVisible = prayersDiv.classList.toggle("hide-beads");
   toggleBeadsBtn.textContent = beadsVisible ? "Show Beads" : "Hide Beads";
 });
+
